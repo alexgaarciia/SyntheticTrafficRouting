8521bf225b0a2cbfda2b4752bdffc9f7343e31ff
algar <al.garcia636@gmail.com>
2024-10-12 23:03:31 +0200

    data pipeline and datasets

---
+++ main.py
@@ -0,0 +1,9 @@
+import re
+import pandas as pd
+
+# Step 1: Read file
+file_path = 'directed-abilene-zhang-5min-over-6months-ALL-native/demandMatrix-abilene-zhang-5min-20040301-0000.txt'
+with open(file_path, 'r') as f:
+    pd.
+    lines = f.readlines()
+
