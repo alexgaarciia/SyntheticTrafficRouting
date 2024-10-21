b2e14ff24fe01c73a5051843a190f4b328277b03
algar <al.garcia636@gmail.com>
2024-10-21 10:24:55 +0200

    added comments

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def process_network_traffic_data(path):
     return df
 
 
+# Test the preprocessing pipeline
 data_path = 'data/demandMatrix-abilene-zhang-5min-20040910-2325.xml'
 demands_df = process_network_traffic_data(data_path)
 print(demands_df)
-
