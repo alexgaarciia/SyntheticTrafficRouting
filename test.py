0f6dd506f277d0040c06cec8e9a49e98709f7711
algar <al.garcia636@gmail.com>
2024-10-26 10:25:47 +0200

    added test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from network_traffic_data_processing import process_network_traffic_data
+from network_traffic_data_processing import process_network_traffic_data, data_preparation
 
 # Test the processing pipeline
 data_path = 'data/demandMatrix-abilene-zhang-5min-20040910-2325.xml'
 demands_df = process_network_traffic_data(data_path)
-print(demands_df)
+final_data = data_preparation(demands_df)
+print(final_data)
